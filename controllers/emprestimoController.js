96706408bad8aedf316e47a9d9e73611edb03cad
Huênio <hueniohugomoura@gmail.com>
2024-11-08 21:25:13 -0300

    correção no arquivo .env

--- controllers/emprestimoController.js
+++ controllers/emprestimoController.js
@@ -1,10 +1,9 @@
 export const createEmprestimo = (req, res) => {
-    res.send('Empréstimo registrado com sucesso');
-  };
-  
+  res.send("Empréstimo registrado com sucesso");
+};
+
 export const getEmprestimos = (req, res) => {
-    res.send('Lista de empréstimos');
-  };
-  
-  // Outros métodos CRUD para empréstimos...
-  
+  res.send("Lista de empréstimos");
+};
+
+// Outros métodos CRUD para empréstimos...
--- controllers/livrosController.js
+++ controllers/livrosController.js
@@ -1,14 +1,13 @@
-import livroSchema from '../validators/livrosValidator.js';
-  
+import livroSchema from "../validators/livrosValidator.js";
+
 export const createLivro = (req, res) => {
-    // Lógica para adicionar um livro
-    res.send('Livro adicionado com sucesso');
-  };
-  
+  // Lógica para adicionar um livro
+  res.send("Livro adicionado com sucesso");
+};
+
 export const getLivros = (req, res) => {
-    // Lógica para listar livros
-    res.send('Lista de livros');
-  };
-  
-  // Outros métodos CRUD (getLivroById, updateLivro, deleteLivro)...
-  
+  // Lógica para listar livros
+  res.send("Lista de livros");
+};
+
+// Outros métodos CRUD (getLivroById, updateLivro, deleteLivro)...
